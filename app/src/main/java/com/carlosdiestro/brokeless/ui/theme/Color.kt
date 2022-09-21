65c4481061edd591e5a3ce2284dfb4ca9b3438e4
Carlos Diestro <carlostroller99@gmail.com>
2022-09-21 16:17:23 +0200

    chore: add app colors

--- app/src/main/java/com/carlosdiestro/brokeless/ui/theme/Color.kt
+++ app/src/main/java/com/carlosdiestro/brokeless/ui/theme/Color.kt
@@ -2,10 +2,9 @@ package com.carlosdiestro.brokeless.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Blue90 = Color(0xFF04041A)
+val Blue70 = Color(0xFF02316A)
+val Blue50 = Color(0XFF0082FA)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Shadow50 = Color(0XFF9B999A)
+val Shadow10 = Color(0XFFFAFAFA)
