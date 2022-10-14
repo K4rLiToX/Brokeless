4be57b58542c65d10597f5e028e8d910d06b1e10
Carlos Diestro <carlostroller99@gmail.com>
2022-10-14 12:49:21 +0200

    feat: add monthly transaction card

---
+++ app/src/main/java/com/carlosdiestro/brokeless/wallet/ui/models/MonthlyTransactionPLO.kt
@@ -0,0 +1,18 @@
+package com.carlosdiestro.brokeless.wallet.ui.models
+
+import com.carlosdiestro.brokeless.core.ui.models.CategoryPLO
+import com.carlosdiestro.brokeless.core.ui.models.CurrencyPLO
+
+class MonthlyTransactionPLO(
+    val id: Int,
+    val concept: String,
+    val category: CategoryPLO,
+    val isActive: Boolean,
+    private val quantity: Double,
+    private val currency: CurrencyPLO
+) {
+    val quantityText: String
+        get() {
+            return if (currency.goesFirst) "${currency.symbol}$quantity" else "$quantity${currency.symbol}"
+        }
+}
