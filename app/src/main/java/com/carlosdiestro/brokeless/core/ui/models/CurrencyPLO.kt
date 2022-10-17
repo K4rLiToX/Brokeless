0cc1da22d698e55dd74091d8eedc01562ff9d0c8
Carlos Diestro <carlostroller99@gmail.com>
2022-10-17 14:11:07 +0200

    feat: modelize new variables

--- app/src/main/java/com/carlosdiestro/brokeless/core/ui/models/CurrencyPLO.kt
+++ app/src/main/java/com/carlosdiestro/brokeless/core/ui/models/CurrencyPLO.kt
@@ -3,8 +3,9 @@ package com.carlosdiestro.brokeless.core.ui.models
 import androidx.annotation.DrawableRes
 
 class CurrencyPLO(
-    val symbol: String,
+    val id: Int,
     val name: String,
+    val symbol: String,
     val goesFirst: Boolean,
     @DrawableRes val iconId: Int
 )
